74df1675f789d1eb6323004892a36e85512e1db8
Erlend Ellingsen <erlend.ame@gmail.com>
2016-12-19 01:59:35 +0100

    Update

--- start.php
+++ start.php
@@ -1,4 +1,4 @@
 <?php
-    $res = exec('sudo forever start /home/pi/work/nodescript/RearViewController/app.js');
+    $res = exec('forever start /home/pi/work/nodescript/RearViewController/app.js');
     var_dump($res);
 ?>
