521dce307e29c979467ccc3c20548b5eb137fd4d
Erlend Ellingsen <erlend.ame@gmail.com>
2016-12-19 01:42:10 +0100

    Update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    $last_file = file_get_contents('last_number.txt');
-    $path_to_image = 'rearclient/images/' . $last_file . '_image.jpg';
+    $last_file = file_get_contents('../rearclient/last_number.txt');
+    $path_to_image = '../rearclient/images/' . $last_file . '_image.jpg';
     
     header('Content-Type: image/jpeg');
     readfile($path_to_image);
